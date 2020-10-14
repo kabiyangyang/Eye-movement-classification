8ed7eb4e77ae172ec465dbba23c17a9797c8fdd2
kabiyangyang <33121382+kabiyangyang@users.noreply.github.com>
2020-10-14 13:11:17 +0200

    Update flow_calculation.py

--- feature_extraction/flow_calculation.py
+++ feature_extraction/flow_calculation.py
@@ -9,7 +9,7 @@ Created on Sun Jun 28 17:22:35 2020
 """
 
 
-#import cv2
+import cv2
 import numpy as np
 
 
@@ -17,10 +17,12 @@ import numpy as np
 def find_flow(mask, path_f, frame, tmp_posi, length):
             final_flow_x = []
             final_flow_y = []
-
+            fin_dist = []
             fore_pos = np.argwhere(mask!=0)
             dist = np.linalg.norm(np.abs(fore_pos[:, [1,0]] - tmp_posi), axis = 1)
             vel_flow = np.ones(length) 
+            
+            #dist = dist[dist<150]
                 
             if(len(dist)!=0):   
                 val = mask[fore_pos[np.argmin(dist)][0], fore_pos[np.argmin(dist)][1]]
@@ -36,12 +38,65 @@ def find_flow(mask, path_f, frame, tmp_posi, length):
                 tmp_vel_y = np.mean(res2)
                 final_flow_x.extend(vel_flow* tmp_vel_x/length)
                 final_flow_y.extend(vel_flow*tmp_vel_y/length)
+                fin_dist.extend(vel_flow * np.min(dist))
                 
             else:
                 final_flow_x.extend(vel_flow*0)
                 final_flow_y.extend(vel_flow*0)
+                fin_dist.extend(vel_flow*0)
             
-            return final_flow_x, final_flow_y
+            return final_flow_x, final_flow_y, fin_dist
+        
+
+def find_flow_similar(mask, path_f, frame, tmp_posi, length, vel):
+            final_flow_x = []
+            final_flow_y = []
+            fin_dist = []
+            fore_pos = np.argwhere(mask!=0)
+            dist = np.linalg.norm(np.abs(fore_pos[:, [1,0]] - tmp_posi), axis = 1)
+            vel_flow = np.ones(length) 
+            
+            #dist = dist[dist<150]
+                
+            if(len(dist)!=0):   
+                tmp_vals = mask[fore_pos[dist<150][:,0], fore_pos[dist<150][:,1]]
+                vals = np.unique(tmp_vals)
+                    
+    
+                path_of = path_f + "{:0>5d}".format(frame) + ".flo"
+                
+                flow_x = read(path_of)[:,:,0]
+                flow_y = read(path_of)[:,:,1]
+                flow_vel = []
+                tmp_vel_x = 0
+                tmp_vel_y = 0
+                if(vals.size != 0):
+                    for val in vals:
+                        res = flow_x[np.argwhere(mask==val)[:,0], np.argwhere(mask==val)[:,1]]
+                        res2 = flow_y[np.argwhere(mask==val)[:,0], np.argwhere(mask==val)[:,1]]
+                        tmp_vel_x = np.mean(res)
+                        tmp_vel_y = np.mean(res2)
+                        flow_vel.append([tmp_vel_x, tmp_vel_y])
+                
+                    vel_dist = np.linalg.norm(flow_vel - vel, axis = 1)
+                    res_flow_x = flow_vel[np.argmin(vel_dist)][0]
+                    res_flow_y = flow_vel[np.argmin(vel_dist)][1]
+                    tmp_val = tmp_vals[np.argmin(vel_dist)]
+                    final_flow_x.extend(vel_flow* res_flow_x /length)
+                    final_flow_y.extend(vel_flow* res_flow_y/length)
+                    fin_dist.extend(vel_flow * np.min(dist[dist<150][tmp_vals == tmp_val]))
+                else:
+                    final_flow_x.extend(vel_flow*0)
+                    final_flow_y.extend(vel_flow*0)
+                    fin_dist.extend(vel_flow * (-1))
+                    
+                
+            else:
+                final_flow_x.extend(vel_flow*0)
+                final_flow_y.extend(vel_flow*0)
+                fin_dist.extend(vel_flow*(-1))
+            
+            return final_flow_x, final_flow_y, fin_dist
     
     
     
